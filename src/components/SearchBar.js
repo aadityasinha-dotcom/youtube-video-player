fa855268ac4da9683c6f8553af8b5c9b9272afbb
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-10-19 22:05:45 +0530

    changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,6 @@ class SearchBar extends React.Component {
         console.log(event.target.value);
     }
 
-    onInputClick () {
-        console.log("Input is clicked");
-    }
-
 
     render() {
         return (
@@ -16,11 +12,7 @@ class SearchBar extends React.Component {
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input 
-                            type="text" 
-                            onClick={this.onInputClick} 
-                            onChange={this.onInputChange} 
-                        />
+                        <input type="text" onChange={this.onInputChange} />
                     </div>
                 </form>
             </div>
