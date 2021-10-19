8e3cc10825778527fbc8090b9e99f47fb2c94ceb
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-10-19 22:04:49 +0530

    added on click and on change functions

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,26 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    onInputChange (event) {
+        console.log(event.target.value);
+    }
+
+    onInputClick () {
+        console.log("Input is clicked");
+    }
+
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                            onClick={this.onInputClick} 
+                            onChange={this.onInputChange} 
+                        />
                     </div>
                 </form>
             </div>
