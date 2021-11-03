cf2f05e9c292ae24e8ad5d0e6a31c3693fdd8ca6
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-11-03 21:05:00 +0530

    validator

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -14,6 +14,11 @@ class SearchBar extends React.Component {
                             values={this.state.term} 
                             onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })} 
                         />
+                        <Validator />
+                        <link
+                            rel="stylesheet"
+                            href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+                        />
                     </div>
                 </form>
             </div>
