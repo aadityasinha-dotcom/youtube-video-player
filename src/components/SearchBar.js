a1b33eddfba5aa929a367400cc826a2e950cb410
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-11-04 19:52:51 +0530

    changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -12,12 +12,7 @@ class SearchBar extends React.Component {
                         <input 
                             type="text" 
                             values={this.state.term} 
-                            onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })} 
-                        />
-                        <Validator />
-                        <link
-                            rel="stylesheet"
-                            href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.4.1/semantic.min.css"
+                            onChange={(e) => this.setState({ term: e.target.value })} 
                         />
                     </div>
                 </form>
