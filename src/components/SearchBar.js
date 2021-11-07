d1708b8294b39f27f95dcaa2eb0d4e736da7b859
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-11-07 18:24:36 +0530

    changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,7 @@ import React from "react";
 class SearchBar extends React.Component {
     state = { term: '' };
 
-    onFormSubmit(event) {
+    onFormSubmit = (event) => {
         event.preventDefault();  //page does'nt refresh after pressing enter
 
         console.log(this.state.term);
