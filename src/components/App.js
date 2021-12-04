4cd3c655df8e39582b736bead1f5ac9ac11065bc
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-12-04 23:11:01 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -9,9 +9,6 @@ class App extends React.Component {
             headers: {
                 Authorization: 'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',     //client id
             }
-        })
-        .then((response) => {
-            console.log(response.data.results);
         });
     }
 
