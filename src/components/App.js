1bb9158ebeca03caef916144dde904db13eb121e
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-12-05 21:26:36 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
         this.setState({ images: response.data.results });
     }
 
+    
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
