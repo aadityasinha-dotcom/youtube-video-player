9785b34da5d5bcfded57201a86cf66ef919291e0
“aadityasinha-dotcom” <aadityasinha009@gmail.com>
2021-12-05 19:46:52 +0530

    url

--- src/components/unsplash.js
+++ src/components/unsplash.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.create({
+export default axios.create({
     baseURL: 'http://api.unsplash.com',
     headers: {
         Authorization: 'Client-ID 2b98c1afb0aed3b3d94a1866bdc3ac013d21a0c86d236a0fee32355c331c0296',     //client id
