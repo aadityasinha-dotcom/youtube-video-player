bec9991b5f6080e37af4d702f9de2c7169e8a0bb
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-12-07 21:58:41 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
-import ImageList from "./imageList";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
     state = { images: null };
