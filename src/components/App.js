34bd45f16d230961d417b8cecef1840c2b2c5fb7
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-12-07 21:59:43 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -19,7 +19,7 @@ class App extends React.Component {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSumbit={this.onSearchSubmit} />
-                <ImageList />
+                <ImageList images={this.state.images} />
             </div>
         );
     }
