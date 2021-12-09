b3246f4940aaa4f616c423e476810216d825fb53
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-12-09 20:51:38 +0530

    changes

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -4,8 +4,7 @@ const ImageList = (props) => {
     return images = props.images.map((image) => {
         return <img src={image.url.regular} />
     });
-
-    return <div>ImageList</div>
+    
 };
 
 export default ImageList;
