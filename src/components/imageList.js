3fa2b888df31f682bc8e0389eec61b84bf11cc27
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-12-12 19:38:57 +0530

    changes

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const ImageList = (props) => {
     const images = props.images.map((image) => {
-        return <img alt={} key={image.id} src={image.url.regular} />
+        return <img alt={image.description} key={image.id} src={image.url.regular} />
     });
     
     return <div>{images}</div>
