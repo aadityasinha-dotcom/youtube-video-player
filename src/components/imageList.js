2df37a3851c6229129aa3c0f296a820ba8a18b56
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-12-12 19:51:02 +0530

    mapping

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 const ImageList = (props) => {
-    const images = props.images.map((image) => {
-        return <img alt={image.description} key={image.id} src={image.url.regular} />
+    const images = props.images.map(({ description, id, urls }) => {
+        return <img alt={description} key={id} src={url.regular} />
     });
     
     return <div>{images}</div>
