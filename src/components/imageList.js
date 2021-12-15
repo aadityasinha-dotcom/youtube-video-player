bd73156178568dc83c874a1095354216d01d2a48
aadityasinha-dotcom <aadityasinha009@gmail.com>
2021-12-15 22:03:58 +0530

    css

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -6,7 +6,7 @@ const ImageList = (props) => {
         return <img alt={description} key={id} src={url.regular} />
     });
     
-    return <div className='Image-List'>{images}</div>
+    return <div className='image-list'>{images}</div>
 };
 
 export default ImageList;
