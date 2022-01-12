4a1a5009325aaa70a5cfd374f38fe7e45558443c
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-12 21:01:30 +0530

    image card

---
+++ src/components/ImageCard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+    render() {
+        return (
+            <div>
+                <img 
+                    alt={this.props.image.description} 
+                    src={this.props.image.urls.regular}
+                />
+            </div>
+        );
+    }
+}
+
+export default ImageCard;
