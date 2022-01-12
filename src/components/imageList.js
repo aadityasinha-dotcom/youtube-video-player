9007be1a123fa5d5884dc4ef801b4b9424168ea3
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-12 21:04:28 +0530

    image card

--- src/components/imageList.js
+++ src/components/imageList.js
@@ -4,7 +4,7 @@ import ImageCard from './ImageCard';
 
 const ImageList = (props) => {
     const images = props.images.map((image) => {
-        return <ImageCard key={id} image={image} />
+        return <ImageCard key={image.id} image={image} />
     });
     
     return <div className='image-list'>{images}</div>
