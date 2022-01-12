5081dfa9db6137792bfd88e8f016e00134803f60
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-12 21:07:12 +0530

    image card

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 class ImageCard extends React.Component {
     render() {
+        const { description, urls } = this.props.image;
+
         return (
             <div>
                 <img 
-                    alt={this.props.image.description} 
-                    src={this.props.image.urls.regular}
+                    alt={description} 
+                    src={urls.regular}
                 />
             </div>
         );
