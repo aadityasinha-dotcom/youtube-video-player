71d01ae2dbc468ea972312fe22a24f9ce3c4d0b1
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-15 18:02:00 +0530

    changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,14 +3,15 @@ import React from "react";
 class SearchBar extends React.Component {
     state = { term = ' ' };
 
-    onInputChange = (event) => {
+    onInputChange = (event) => {      //method to take input
         this.setState({ term: event.target.value });
     };
+
 
     render () {
         return (
             <div className="search-bar ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Video Search</label>
                         <input 
