561ef16d9e4e1c78082ee82458f9820077b435cb
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-15 17:58:08 +0530

    changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    state = { term = ' ' };
+
     render () {
         return (
             <div className="search-bar ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Video Search</label>
-                        <input type="text" />
+                        <input type="text" value={this.state.term} />
                     </div>
                 </form>
             </div>
