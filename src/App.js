fe9eb6651ffd41d7c9cd8e099115c35d21383e72
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-15 17:48:18 +0530

    search bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 class App extends React.Component {
   render () {
-    return <div>App</div>;
+    return (
+      <div><SearchBar /></div>
+    );
   }
 }
 
