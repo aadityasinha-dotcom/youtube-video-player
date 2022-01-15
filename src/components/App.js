d7f1790bce73be4cdad5ea5d864e12a5da3c7be6
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-15 21:27:28 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  onTermSubmit = () => {
+
+  };
+
   render () {
     return (
       <div><SearchBar /></div>
