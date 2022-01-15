42c114ef960dfb1cfc32d49a793f41dfffa5164e
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-15 18:02:30 +0530

    changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,7 +7,9 @@ class SearchBar extends React.Component {
         this.setState({ term: event.target.value });
     };
 
-
+    onFormSubmit = (event) => {   //method to submit input
+        
+    }
     render () {
         return (
             <div className="search-bar ui segment">
