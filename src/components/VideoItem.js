c5c194551efe0c54aedbcd6402e5fd117e86be58
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-16 15:37:42 +0530

    nesting callbacks

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './VideoItem.css';
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-  <div className="video-item item">
+  <div onCick={() => onVideoSelect(video)} className="video-item item">
     <img className="ui image" src={video.snippet.thumbnails.medium.url} />
     <div className="content">
         <div className="header">
