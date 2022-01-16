77b80c73d96fa1e6c3998bec12f8bf743d3d338a
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-16 15:46:25 +0530

    rendering

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const VideoDetail = ({video}) => {
+    if(!video) {
+        return <div>Loading...</div>
+    }
+    return <div>{video.snippet.title}</div>
+};
+
+export default VideoDetail;
